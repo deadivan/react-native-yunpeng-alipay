1d94d33ace474d00aa14e77f42467346bfb1dc29
deadivan <deadivan@gmail.com>
2018-01-12 11:29:05 +0800

    remove extra override

--- android/src/main/java/com/yunpeng/alipay/AlipayPackage.java
+++ android/src/main/java/com/yunpeng/alipay/AlipayPackage.java
@@ -22,7 +22,6 @@ public class AlipayPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
